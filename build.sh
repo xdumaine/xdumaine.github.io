244448a881be697d618e2b8ac9a03ea0964aa3c3
Xander Dumaine <xander.dumaine@inin.com>
2015-04-30 17:09:06 -0400

    Add build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -rf gh-pages
+npm install
+bower install
+broccoli build gh-pages
+rm -rf node_modules bower_components dist tmp bower.json Brocfile.js build.sh LICENSE package.json README.md app
+cp -r gh-pages/* ./
+git checkout master
