165e223a18428048245764b464bd6fe8fde8ca8f
Xander Dumaine <xander.dumaine@inin.com>
2015-05-03 17:27:11 -0400

    Add figureset to cycle through figures for a project

--- app/src/app.js
+++ app/src/app.js
@@ -3,7 +3,29 @@ import Test from './test';
 class App {
     constructor() {
         Test.test();
-        setTimeout(() => window.location = 'http://www.xdumaine.com', 3000);
+        //setTimeout(() => window.location = 'http://blog.xdumaine.com', 3000);
+
+        $('.figure-set').each((i, figureSet) => {
+            $(figureSet).find('figure:not(:first)').addClass('collapse');
+            $(figureSet).find('.previous').click((i, previous) => {
+                var $prev = $(figureSet).find('figure:not(.collapse)').prev('figure');
+                if (!$prev || !$prev.length) {
+                    $prev = $(figureSet).find('figure').last();
+                }
+                $(figureSet).find('figure:not(.collapse)').addClass('collapse');
+                $prev.removeClass('collapse');
+                return false;
+            });
+            $(figureSet).find('.next').click((i, next) => {
+                var $next = $(figureSet).find('figure:not(.collapse)').next('figure');
+                if (!$next || !$next.length) {
+                    $next = $(figureSet).find('figure').first();
+                }
+                $(figureSet).find('figure:not(.collapse)').addClass('collapse');
+                $next.removeClass('collapse');
+                return false;
+            });
+        });
     }
 }
 
