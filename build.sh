65fe210948379a33a081ce9afec69e46a287a185
Xander Dumaine <xander.dumaine@inin.com>
2015-05-03 21:01:58 -0400

    update build and gitignore for npm-debug.log

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -rf gh-pages
 npm install
 bower install
 broccoli build gh-pages
-rm -rf dist tmp bower.json Brocfile.js build.sh LICENSE package.json README.md app
+rm -rf dist tmp bower.json Brocfile.js build.sh LICENSE package.json README.md app npm-debug.log
 git checkout master
 cp -r gh-pages/* ./
 rm -rf gh-pages
