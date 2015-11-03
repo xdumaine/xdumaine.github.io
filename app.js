d1be2da6f7a38873eacb2ebc5fe017c6ae4acba6
Xander Dumaine <xander.dumaine@inin.com>
2015-11-02 22:36:30 -0500

    use https

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var App = function App() {
 
 module.exports = new App();
 
-},{"./test":"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-d41Bheno.tmp/src/test.js"}],"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-d41Bheno.tmp/src/test.js":[function(require,module,exports){
+},{"./test":"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-XZSwhCe8.tmp/src/test.js"}],"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-XZSwhCe8.tmp/src/test.js":[function(require,module,exports){
 "use strict";
 
 module.exports = {
