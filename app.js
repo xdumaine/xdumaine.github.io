2b556b6f9e37ecba0690c86531c1e3b71a0452c0
Xander Dumaine <xander.dumaine@inin.com>
2015-11-06 10:27:39 -0500

    update from develop (iconography and youtube link to presentation)

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var App = function App() {
 
 module.exports = new App();
 
-},{"./test":"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-1Mi4gddL.tmp/src/test.js"}],"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-1Mi4gddL.tmp/src/test.js":[function(require,module,exports){
+},{"./test":"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-HI0Gzq90.tmp/src/test.js"}],"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-HI0Gzq90.tmp/src/test.js":[function(require,module,exports){
 "use strict";
 
 module.exports = {
