dba5b25cee0b13986a734d73ed0a32ef26aa5352
Xander Dumaine <xander.dumaine@inin.com>
2015-11-08 21:11:07 -0500

    build latest

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var App = function App() {
 
 module.exports = new App();
 
-},{"./test":"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-K3iqgiTz.tmp/src/test.js"}],"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-K3iqgiTz.tmp/src/test.js":[function(require,module,exports){
+},{"./test":"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-gjQpa5UI.tmp/src/test.js"}],"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-gjQpa5UI.tmp/src/test.js":[function(require,module,exports){
 "use strict";
 
 module.exports = {
