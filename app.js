dd95c6ef57a7f7ceb15339e5a3024c6c6029fd6f
Xander Dumaine <xander.dumaine@inin.com>
2015-12-21 20:16:16 -0500

    latest changes YOLO

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var App = function App() {
 
 module.exports = new App();
 
-},{"./test":"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-FmxHJF5v.tmp/src/test.js"}],"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-FmxHJF5v.tmp/src/test.js":[function(require,module,exports){
+},{"./test":"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-ThBrULCa.tmp/src/test.js"}],"/Users/xanderdumaine/Code/personal/xdumaine.github.io/tmp/babel-tmp_dest_dir-ThBrULCa.tmp/src/test.js":[function(require,module,exports){
 "use strict";
 
 module.exports = {
