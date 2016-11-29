b737754106181a0f3b1b3a619ad1d43ca9a96f9b
Xander Dumaine <xander.dumaine@inin.com>
2016-11-29 13:46:21 -0500

    use npm module to build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -rf gh-pages
 npm install
 bower install
-broccoli build gh-pages
+node_modules/.bin/broccoli build gh-pages
 rm -rf dist tmp bower.json Brocfile.js build.sh LICENSE package.json README.md app npm-debug.log
 git checkout master
 cp -r gh-pages/* ./
