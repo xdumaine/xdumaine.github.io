4a3acd95104ab6838fe3fbb4c0d947b1db7ce58d
Xander Dumaine <xander.dumaine@inin.com>
2017-01-24 23:09:31 -0500

    uglify

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ rm -rf gh-pages
 npm install
 bower install
 node_modules/.bin/broccoli build gh-pages
+node_modules/.bin/uglifyjs gh-pages/app.js -o gh-pages/app.js
 rm -rf dist tmp bower.json Brocfile.js build.sh LICENSE package.json README.md app npm-debug.log
 git checkout master
 cp -r gh-pages/* ./
