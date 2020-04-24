075eac8f8b48b79d2ba3b21204a1544588f27ea5
Xander Dumaine <xander.dumaine@lifeomic.com>
2020-04-24 15:26:19 -0400

    npx bower

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm -rf gh-pages
 npm install
-bower install
+npx bower install
 node_modules/.bin/broccoli build gh-pages
 node_modules/.bin/uglifyjs gh-pages/app.js -o gh-pages/app.js
 rm -rf dist tmp bower.json Brocfile.js build.sh LICENSE package.json README.md app npm-debug.log
